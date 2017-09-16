d4b0ab3447545ae3e3ca7973bea9102dd16fed6c
Matthias Rost <mrost@inet.tu-berlin.de>
2017-09-16 14:49:40 +0200

    initial commit

---
+++ cli.py
@@ -0,0 +1,367 @@
+import click
+import sys, os
+import cPickle
+import time
+import multiprocessing
+import math
+
+import datamodel as dm
+import experiment_execution as ee
+import postprocess_solutions as pps
+
+
+sys.path.insert(0, os.path.abspath('../'))
+
+
+@click.group()
+def cli():
+    pass
+
+
+#default parameters are based on the sigmetrics publication (2016)
+@cli.command()
+@click.argument('instance_storage_name')
+@click.argument('seed', type=click.INT)
+@click.option('--min_number_nodes', type=click.INT, default=10)
+@click.option('--max_number_nodes', type=click.INT, default=30)
+@click.option('--number_nodes_step', type=click.INT, default=1)
+@click.option('--min_number_wps', type=click.INT, default=1)
+@click.option('--max_number_wps', type=click.INT, default=3)
+@click.option('--number_wps_step', type=click.INT, default=1)
+@click.option('--number_of_repetitions', type=click.INT, default=200)
+def generate_instances(instance_storage_name,
+                       seed,
+                       min_number_nodes,
+                       max_number_nodes,
+                       number_nodes_step,
+                       min_number_wps,
+                       max_number_wps,
+                       number_wps_step,
+                       number_of_repetitions):
+
+    nodes_list = [x for x in range(min_number_nodes, max_number_nodes+1, number_nodes_step)]
+    wps_list = [x for x in range(min_number_wps, max_number_wps+1, number_wps_step)]
+    index_list = [x for x in range(0, number_of_repetitions)]
+
+    if len(nodes_list) == 0:
+        raise Exception("Cannot create instances with min_number_nodes: {}, max_number_nodes: {}, number_nodes_step: {}".format(min_number_nodes,
+                                                                                                                                max_number_nodes,
+                                                                                                                                number_nodes_step))
+
+    if len(wps_list) == 0:
+        click.confirm('"Instances will be generated WITHOUT any waypoints present. Do you want to continue?', abort=True)
+
+    if len(index_list) == 0:
+        raise Exception("Cannot create instances with number_of_repetitions: {}".format(number_of_repetitions))
+
+    instances_generation_parameters = dm.InstanceGenerationParameters(nodes=nodes_list,
+                                                                     number_wps=wps_list,
+                                                                     index=index_list)
+
+
+
+    output_filename, instance_storage = f_generate_instances(instance_storage_name,
+                                                             seed,
+                                                             raw_instance_generation_parameters=instances_generation_parameters)
+
+
+    with open(output_filename, "w") as f:
+        print "\nWriting the instance storage container with id {} into file {}".format(instance_storage.identifier, output_filename)
+        cPickle.dump(instance_storage, f)
+        print "\nFile {} was written".format(output_filename)
+
+
+def f_generate_instances(instance_storage_name,
+                         seed,
+                         raw_instance_generation_parameters):
+
+    output_filename = instance_storage_name + ".cpickle"
+
+    output_string = ""
+
+    output_string += "Generation of instances of {} is done according to the following parameters\n".format(output_filename)
+    output_string += "\t seed: {}\n".format(seed)
+    output_string += "\t instance generation parameters are...\n".format()
+    output_string += "\t\t nodes:      {}\n".format(raw_instance_generation_parameters.nodes)
+    output_string += "\t\t number_wps: {}\n".format(raw_instance_generation_parameters.number_wps)
+    output_string += "\t\t indices:    {}\n\n".format(raw_instance_generation_parameters.index)
+
+
+
+    print output_string
+
+    with open(instance_storage_name + ".txt", "w") as f:
+        f.write(output_string)
+    print output_string
+
+    print "\nStarting generation of instances..."
+
+    instance_storage_id = output_filename + "[{}]".format(time.strftime("%d/%m/%Y--%H:%S"))
+
+    instance_storage = dm.InstanceStorage(identifier=instance_storage_id,
+                                      instance_generation_parameters=raw_instance_generation_parameters,
+                                      seed=seed)
+    instance_storage.generate()
+
+    print "\n...generated {} many instances.".format(instance_storage.number_of_instances)
+
+    return output_filename, instance_storage
+
+CONSTANT_YES = "yes"
+CONSTANT_NO = "no"
+CONSTANT_BOTH = "both"
+
+def from_yes_no_both_to_list(string_value):
+    result = []
+    if CONSTANT_YES == string_value or CONSTANT_BOTH == string_value:
+        result.append(True)
+    if CONSTANT_NO == string_value or CONSTANT_BOTH == string_value:
+        result.append(False)
+    return result
+
+def check_input_range_execution_parameters(slice_to_execute, number_of_slices, timelimit, threads, mip_gap):
+    if slice_to_execute < 0 or slice_to_execute >= number_of_slices:
+        raise Exception("The slice to be executed must be in the range [0,1,...,number_of_slices-1]")
+
+    if timelimit < 1:
+        raise Exception("Timelimit must be greater than 0.")
+    if threads <= 0 or threads > multiprocessing.cpu_count():
+        raise Exception("Thread parameter must lie in the interval [1,...,{}].".format(multiprocessing.cpu_count()))
+    if mip_gap < 0 or mip_gap > 1:
+        raise Exception("mip_gap parameter must be in the interval [0,1]")
+
+@cli.command()
+@click.argument('instance_storage_filename')
+@click.argument('output_base_name')
+@click.argument('slice_to_execute', type=click.INT)
+@click.argument('number_of_slices', type=click.INT)
+@click.argument('decision_variant', type=click.Choice([CONSTANT_YES, CONSTANT_NO, CONSTANT_BOTH]))
+@click.argument('strong_loop_freedom', type=click.Choice([CONSTANT_YES, CONSTANT_NO, CONSTANT_BOTH]))
+@click.argument('flow_extension', type=click.Choice([CONSTANT_YES, CONSTANT_NO, CONSTANT_BOTH]))
+@click.option('--timelimit', type=click.INT, default=600)
+@click.option('--threads', type=click.INT, default=1)
+@click.option('--mip_gap', type=click.FLOAT, default=0.01)
+def execute_experiments(instance_storage_filename,
+                        output_base_name,
+                        slice_to_execute,
+                        number_of_slices,
+                        decision_variant,
+                        strong_loop_freedom,
+                        flow_extension,
+                        timelimit=None,
+                        threads=None,
+                        mip_gap=None):
+
+    check_input_range_execution_parameters(slice_to_execute, number_of_slices, timelimit, threads, mip_gap)
+
+    decision_choice_list = from_yes_no_both_to_list(decision_variant)
+    strong_loop_freedom_list = from_yes_no_both_to_list(strong_loop_freedom)
+    flow_extension_list = from_yes_no_both_to_list(flow_extension)
+
+    f_execute_experiments(instance_storage_filename=instance_storage_filename,
+                          output_base_name=output_base_name,
+                          slice_to_execute=slice_to_execute,
+                          number_of_slices=number_of_slices,
+                          decision_variant_choices=decision_choice_list,
+                          strong_loop_freedom_choices=strong_loop_freedom_list,
+                          flow_extension_choices=flow_extension_list,
+                          timelimit=timelimit,
+                          threads=threads,
+                          mip_gap=mip_gap)
+
+
+
+def f_execute_experiments(instance_storage_filename,
+                          output_base_name,
+                          slice_to_execute,
+                          number_of_slices,
+                          decision_variant_choices,
+                          strong_loop_freedom_choices,
+                          flow_extension_choices,
+                          timelimit=None,
+                          threads=None,
+                          mip_gap=None):
+    instance_storage = None
+    with open(instance_storage_filename, "r") as f:
+        print "\nReading instance storage from {}".format(instance_storage_filename)
+        instance_storage = cPickle.load(f)
+        print "\t..done."
+
+
+    model_configurations = []
+    for decision_value in decision_variant_choices:
+        for slf_value in strong_loop_freedom_choices:
+            for flow_extension_value in flow_extension_choices:
+                model_configurations.append(dm.ModelConfiguration(decision_variant=decision_value,
+                                                                  strong_loop_freedom=slf_value,
+                                                                  use_flow_extension=flow_extension_value))
+
+
+
+    gurobi_settings = dm.GurobiSettings(timelimit=timelimit,
+                                        threads=threads,
+                                        mip_gap=mip_gap)
+
+    executor = ee.ExperimentExecutor(instance_storage,
+                                     slice_to_execute,
+                                     number_of_slices,
+                                     model_configurations_to_execute=model_configurations,
+                                     gurobi_settings=gurobi_settings)
+
+    print "starting execution of instances.."
+    executor.execute_all_instances()
+    print "\t..done. All experiments are finished."
+    executor.pickle_experiment_storage(output_base_name)
+
+
+
+@cli.command()
+@click.argument('filename_to_write')
+@click.argument('instance_storage_filename')
+@click.argument('output_base_name')
+@click.argument('number_of_processes', type=click.INT)
+@click.argument('decision_variant', type=click.Choice([CONSTANT_YES, CONSTANT_NO, CONSTANT_BOTH]))
+@click.argument('strong_loop_freedom', type=click.Choice([CONSTANT_YES, CONSTANT_NO, CONSTANT_BOTH]))
+@click.argument('flow_extension', type=click.Choice([CONSTANT_YES, CONSTANT_NO, CONSTANT_BOTH]))
+@click.option('--timelimit', type=click.INT, default=600)
+@click.option('--threads', type=click.INT, default=1)
+@click.option('--mip_gap', type=click.FLOAT, default=0.01)
+def write_bash_file_for_parallel_execution(filename_to_write,
+                                           instance_storage_filename,
+                                           output_base_name,
+                                           number_of_processes,
+                                           decision_variant,
+                                           strong_loop_freedom,
+                                           flow_extension,
+                                           timelimit=None,
+                                           threads=None,
+                                           mip_gap=None):
+
+    check_input_range_execution_parameters(0, 1, timelimit, threads, mip_gap)
+
+    output_file = "#!/bin/bash\n\n"
+    for i in range(number_of_processes):
+        output_file += "(python cli.py execute_experiments {} {} {} {} {} {} {} " \
+                       "--timelimit {} --threads {} --mip_gap {} | tee -i {}_sub_{}.log) &\n".format(instance_storage_filename,
+                                                                                                     output_base_name,
+                                                                                                     i,
+                                                                                                     number_of_processes,
+                                                                                                     decision_variant,
+                                                                                                     strong_loop_freedom,
+                                                                                                     flow_extension,
+                                                                                                     timelimit,
+                                                                                                     threads,
+                                                                                                     mip_gap,
+                                                                                                     output_base_name,
+                                                                                                     i)
+
+    with open(filename_to_write, 'w') as f:
+        f.write(output_file)
+
+
+
+@cli.command()
+@click.argument("output_filename", type=click.Path())
+@click.argument('files', nargs=-1, type=click.Path())
+def merge_experiment_storages(output_filename, files):
+    resulting_experiment_storage = dm.ExperimentStorage(instance_storage_id="")
+
+    for filename in files:
+        other_experiment_storage = None
+        with open(filename, "r") as f:
+            print "Reading experiment storage from {}".format(filename)
+            other_experiment_storage = cPickle.load(f)
+        resulting_experiment_storage.import_results_from_other_experiment_storage(other_experiment_storage)
+
+    with open(output_filename, "wb") as f:
+        cPickle.dump(resulting_experiment_storage, f)
+
+
+@cli.command()
+@click.argument("experiment_storage_pickle", type=click.Path())
+def print_results_of_experiment_storage(experiment_storage_pickle):
+
+    experiment_storage = None
+    with open(experiment_storage_pickle, "r") as f:
+        experiment_storage = cPickle.load(f)
+
+    output = ""
+    for instance_index in sorted(experiment_storage.contained_instances.keys()):
+        output += "index: {}\n".format(instance_index)
+        for model_configuration_rep in experiment_storage.instance_solutions[instance_index]:
+            solution = experiment_storage.instance_solutions[instance_index][model_configuration_rep]
+            output += "\t\t config: {}\t --> " \
+                      "number_rounds: {};\t " \
+                      "optimal: {};\t " \
+                      "wall clock: {};\t " \
+                      "gurobi: {} \n".format(model_configuration_rep,
+                                             solution.get_number_of_rounds(),
+                                             solution.is_optimal(),
+                                             solution.status.runtime_wall_clock,
+                                             solution.status.runtime_gurobi)
+        output += "\n"
+
+    print output
+
+
+
+@cli.command()
+@click.argument("output_filename", type=click.Path())
+@click.argument('files', nargs=-1, type=click.Path())
+def create_instance_storage_for_hard_instances(output_filename,
+                                               files):
+    instance_storage = dm.InstanceStorage(identifier="hard_instances_8_8192",
+                                          instance_generation_parameters="from_file",
+                                          seed=-1)
+
+    for i, file in enumerate(files):
+        print "reading instance from file {}".format(file)
+        instance = dm.NetworkUpdateInstance()
+        instance.read_from_edge_only_file(file)
+
+        upper_bound_on_rounds = int(math.ceil(6.0*math.log(instance.number_of_nodes,2)) + 2)
+
+        instance.rounds = min(upper_bound_on_rounds, instance.rounds)
+
+        instance_storage.contained_instances.append(i)
+        instance_storage.instance_dictionary[i] = instance
+        instance_storage.numeric_index_to_parameter[i] = "from_file"
+
+        instance.write_to_file(file + "new")
+
+
+    instance_storage.number_of_instances = len(instance_storage.contained_instances)
+
+    print instance_storage
+
+    print "writing to {}..".format(output_filename)
+    with open(output_filename, "w") as f:
+        cPickle.dump(instance_storage, f)
+    print "\t..finished writing the file"
+
+
+@cli.command()
+@click.argument("original_experiment_storage_filename", type=click.Path())
+@click.argument("extracted_experiment_data_storage_filename", type=click.Path())
+def create_extracted_experiment_data_storage(original_experiment_storage_filename,
+                                             extracted_experiment_data_storage_filename):
+
+    original_experiment_storage = None
+    with open(original_experiment_storage_filename, "r") as f:
+        original_experiment_storage = cPickle.load(f)
+
+    extracted_experiment_storage = pps.create_extracted_experiment_data_storage(original_experiment_storage)
+    extracted_experiment_storage.collect_instance_generation_parameters()
+    if extracted_experiment_storage.check_completeness_of_data():
+        print "data is complete!"
+    else:
+        raise Exception("Data is missing!")
+
+
+    with open(extracted_experiment_data_storage_filename, "w") as f:
+        cPickle.dump(extracted_experiment_storage, f)
+    print "written extracted experiment data storage to {}".format(extracted_experiment_data_storage_filename)
+
+if __name__ == '__main__':
+    cli()
+
---
+++ tests.py
@@ -0,0 +1,152 @@
+
+
+import datamodel as dm
+import experiment_execution as ee
+import cli
+
+import itertools
+import pytest
+import cPickle
+
+from datamodel import GurobiSettings
+
+
+@pytest.fixture
+def generation_parameters():
+    return dm.InstanceGenerationParameters(nodes=[7,15],
+                                           number_wps=[0,1,2],
+                                           index=[x for x in range(0,3)])
+
+class TestNetworkInstanceStorage():
+
+    def test_generation_function_of_cli(self, generation_parameters):
+        output_filename, instance_storage = cli.f_generate_instances("test_test", 1337, generation_parameters)
+
+        assert instance_storage is not None, "no instance storage returned"
+
+        set_of_triples = set()
+
+        for number_nodes in generation_parameters.nodes:
+            for wps in generation_parameters.number_wps:
+                for index in generation_parameters.index:
+                    set_of_triples.add((number_nodes, wps, index))
+
+
+
+        for instance_key, instance in instance_storage.instance_dictionary.iteritems():
+            assert instance is not None, "instance contained in the storage is none"
+
+            gen_parameter = instance_storage.numeric_index_to_parameter[instance_key]
+
+            generation_triple = (gen_parameter.nodes,
+                                 gen_parameter.number_wps,
+                                 gen_parameter.index)
+
+            assert generation_triple in set_of_triples, "there exists an instance generated according to a specification not contained in the parameter list"
+            set_of_triples.remove(generation_triple)
+
+        assert len(set_of_triples) == 0, "there were more instances generated than specified"
+
+
+
+@pytest.fixture
+def model_configurations_to_execute():
+    result = []
+
+    bool_list = [False, True]
+    for v1, v2, v3 in itertools.product(bool_list, bool_list, bool_list):
+        print v1, v2, v3
+        model_configuration = dm.ModelConfiguration(decision_variant=v1,
+                                                    strong_loop_freedom=v2,
+                                                    use_flow_extension=v3)
+        result.append(model_configuration)
+
+    return result
+
+
+@pytest.fixture
+def instance_storage_small_from_pickle():
+    instance_storage = None
+    with open("test_data/small_test_instance_storage.cpickle", "r") as f:
+        instance_storage = cPickle.load(f)
+    return instance_storage
+
+@pytest.fixture
+def gurobi_settings():
+    return dm.GurobiSettings(timelimit=120, threads=1, mip_gap=0.01)
+
+
+class TestGurobi():
+
+    def test_execution_of_stored_sample_instances_and_sanity_check_solutions(self,
+                                                                             model_configurations_to_execute,
+                                                                             gurobi_settings,
+                                                                             generation_parameters):
+
+        _, instance_storage = cli.f_generate_instances("test_test", 1337, generation_parameters)
+
+        experiment_executor = ee.ExperimentExecutor(instance_storage,
+                                                    0,
+                                                    1,
+                                                    model_configurations_to_execute,
+                                                    gurobi_settings)
+
+        experiment_executor.execute_all_instances()
+
+        experiment_storage = experiment_executor.experiment_storage
+
+        model_configuration_representations = [x.get_simple_tuple_representation() for x in model_configurations_to_execute]
+
+        for instance_index in instance_storage.contained_instances:
+
+            assert instance_index in experiment_storage.instance_solutions
+
+            solutions_of_instance = experiment_storage.instance_solutions[instance_index]
+
+            assert solutions_of_instance is not None
+
+            #check that all solutions agree on feasibility
+
+            print solutions_of_instance.keys()
+            print model_configurations_to_execute[0]
+
+            first_sol = solutions_of_instance[model_configuration_representations[0]]
+            found_sol = first_sol.is_feasible()
+
+            for model_config in model_configuration_representations:
+                assert solutions_of_instance[model_config].is_feasible() == found_sol, "One configuration found a solution while the other didn't"
+
+            optimal_solutions = {}
+
+            if found_sol:
+                #check that optimal solutions have the same objective value or better than non-optimal solutions
+                for config_one in model_configuration_representations:
+                    for config_two in model_configuration_representations:
+                        if config_one[0][1] or config_two[0][1]:
+                            continue
+                        if config_one[2][1] != config_two[2][1]:
+                            continue
+
+                        if solutions_of_instance[config_one].is_optimal():
+                            assert solutions_of_instance[config_one].get_number_of_rounds() <= solutions_of_instance[config_two].get_number_of_rounds(), \
+                                "Optimal number of rounds is larger than non-optimal number of rounds or optimal rounds do no not agree"
+
+                            optimal_solutions[config_one[2][1]] = solutions_of_instance[config_one].get_number_of_rounds()
+
+                if False in optimal_solutions and True in optimal_solutions:
+                    assert optimal_solutions[False] <= optimal_solutions[True], \
+                        "Strong-loop freedom needs less rounds than relaxed-loop freedom (in optimal solutions)"
+
+            else:
+                print "Also contained infeasible scnearios!"
+
+
+
+
+
+
+
+
+
+
+
